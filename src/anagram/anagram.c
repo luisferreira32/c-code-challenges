f0f9b5b2f3d51aa759fb9869d6bc4a6093c2adb6
henriquepsantos <henriquepsantos@gmail.com>
2022-04-21 00:32:59 +0100

    Added 100 primes. Added characters. CHORE - assign each char a number and each word an id. hash.

--- src/anagram/anagram.c
+++ src/anagram/anagram.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
 
 #include "quicksort.h"
+#include "util.h"
+
+int primes[] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61,
+	67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157,
+	163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257,
+	263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367,
+	373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 
+	479, 487, 491, 499, 503, 509, 521, 523, 541};
+
+// ABCDEFGHIJKLMNOPQRSTUVWXYZÁÀÂÃÉÈÊÍÌÎÓÒÔÕÚÙÛ
+char portugues[] = "abcdefghijklmnopqrstuvwxyz"; // áàâã = A; éèê = E; íìî = I; óòôõ = O; úùû = U.
+
+int getHash(char *str, int SIZE)
+{
+	return 0;
+}
 
 /* anagram challenge runner */
 void anagram_mock()
 {
     printf("[INFO] Anagram mock has ran!\n");
-    quicksort_mock();
 }
+
+
