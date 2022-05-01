898f551e80f6179f2b05300f4c30805d5bc55ca0
henriquepsantos <henriquepsantos@gmail.com>
2022-05-01 20:15:07 +0100

    Added solver 3, passes all tests. Lacks documentation :(

--- src/wordinversion/solver3.c
+++ src/wordinversion/solver3.c
@@ -1,40 +1,30 @@
 #include <stdio.h>
 #include "solver3.h"
 #include "util.h"
+#include "quicksort.h"
 
 char *wi_solver3(char *input, int input_size)
 {
-	int L = 1 + (input_size / 2);
-	int spaces[L];
-	for (int i = 0; i < L; ++i) spaces[i] = 0;
-	for (int i = 0; i < input_size; ++i) printf("%c", input[i]);
-	printf("\n");
-	int j = 0;
+	int left_spc = -1, right_spc = input_size;
 	for (int i = 0; i < input_size/2; i++)
 	{
 		char_SWAP(input, i, input_size-i-1);
-
-		if (input[i] == ' ') 
-		{
-			/* printf("-%d", i); */
-			spaces[j] = i;
-			j++;
-			input[i] = '_';
+		if (input[i] == ' ') {
+			char_invertSubArray(input, left_spc+1, i-1);
+			left_spc = i;
 		}
-		if (input[input_size-i-1] == ' ')
-		{
-			//printf("-%d", input_size-i-1);
-			input[input_size-i-1] = '_';
-			spaces[j] = input_size-i-1;
-			j++;
+		if (input[input_size-i-1] == ' ') {
+			char_invertSubArray(input, input_size-i, right_spc-1);
+			right_spc = input_size-i-1;
 		}
-		printf("%d\n", j);
-
-		//printf("%c ", input[i]);
-		//printf("%c ", input[input_size-i-1]);
 	}
-	for (int i = 0; i < input_size; ++i) printf("%c", input[i]);
-	printf("\n");
-	for (int i = 0; i < L; ++i) printf("%d ", spaces[i++]);
+	int middle = input_size/2;
+	if (input[middle] == ' ')
+	{
+		char_invertSubArray(input, ++left_spc, middle-1);
+		char_invertSubArray(input, middle+1, --right_spc);
+	} else {
+		char_invertSubArray(input, ++left_spc, --right_spc);
+	}
 	return input;
 }
