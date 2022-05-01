4df447051b2ddb0621b01901fad9de66a04ce9ef
henriquepsantos <henriquepsantos@gmail.com>
2022-05-01 20:14:28 +0100

    Added util function to invert sub-array.

--- lib/util.c
+++ lib/util.c
@@ -5,7 +5,6 @@ void printArray(int *Array, int size)
 	for (int i = 0; i < size; ++i) printf("[DEBUG] %d\n", Array[i]);
 }
 
-
 void int_SWAP(int *params, int i, int j)
 {
 	int temp = params[i];
@@ -20,3 +19,8 @@ void char_SWAP(char *params, int i, int j)
 	params[j] = temp;
 }
 
+void char_invertSubArray(char *params, int from, int to)
+{
+	int middle = (1 + to - from) / 2;
+	for (int i = 0; i < middle; i++) char_SWAP(params, from+i, to-i);
+}
--- lib/util.h
+++ lib/util.h
@@ -1,6 +1,7 @@
 #pragma once
 
 void printArray(int *Array, int size);
+void char_invertSubArray(char *, int, int);
 
 void char_SWAP(char *params, int i, int j);
 void int_SWAP(int *params, int i, int j);
